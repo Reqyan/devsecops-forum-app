b37d5278e2fc14380435a6badff6868f52ab68d7
KamajayaAriestuta <76470171+KamajayaAriestuta@users.noreply.github.com>
2024-11-10 08:28:21 +0700

    CRUD_post1

--- resources/views/posts/layouts/navbar.blade.php
+++ resources/views/posts/layouts/navbar.blade.php
@@ -41,9 +41,6 @@
             <div class="col-md-6">
                 <div class="navbar-menu-left-side239">
                     <ul>
-                        <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i>Contact</a></li>
-                        <li><a href="#"><i class="fa fa-headphones" aria-hidden="true"></i>Support</a></li>
-
                         <!-- Cek apakah pengguna sudah login -->
                         @if (Auth::check())
                             <!-- Jika sudah login, tampilkan nama pengguna dan tautan ke profil -->
@@ -55,20 +52,6 @@
                             <li><a href="/login"><i class="fa fa-user" aria-hidden="true"></i>Login Area</a></li>
                         @endif
                     </ul>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="navbar-serch-right-side">
-                    <form class="navbar-form" role="search">
-                        <div class="input-group add-on">
-                            <input class="form-control form-control222" placeholder="Search" id="srch-term"
-                                type="text">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default btn-default2913" type="button"><i
-                                        class="glyphicon glyphicon-search"></i></button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
@@ -86,7 +69,7 @@
                     data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle
                         navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span
                         class="icon-bar"></span> </button>
-                <a class="navbar-brand" href="#"><img src="image/logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="#"><img src="/image/logo.png" alt="Logo"></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -94,39 +77,6 @@
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="/">Home</a></li>
                     <li><a href="/create-post">Ask Question</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false">Question <span class="caret"></span></a>
-                        <ul class="dropdown-menu animated zoomIn">
-                            <li><a href="category.html">Question Category</a></li>
-                            <li><a href="category.html">HTML</a></li>
-                            <li><a href="category.html">CSS</a></li>
-                            <li><a href="category.html">Javascript</a></li>
-                            <li><a href="category.html">Bootstrap</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false">Blog <span class="caret"></span></a>
-                        <ul class="dropdown-menu animated zoomIn">
-                            <li><a href="blog.html">Blog </a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false">Page <span class="caret"></span></a>
-                        <ul class="dropdown-menu animated zoomIn">
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="contact_us.html"> Contact Us</a></li>
-                            <li><a href="/create-post"> Ask Question </a></li>
-                            <li><a href="/"> Post-Details </a></li>
-                            <li><a href="/">All User</a></li>
-                            <li><a href="/"> User Question </a></li>
-                            <li><a href="/"> Category </a></li>
-                            <li><a href="#"> 404 </a></li>
-                        </ul>
-                    </li>
-                    <li><a href="contact_us.html">Contact us</a></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
--- resources/views/posts/layouts/welcome.blade.php
+++ resources/views/posts/layouts/welcome.blade.php
@@ -8,15 +8,8 @@
                     <br> habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur vitae velit in neque dictum blandit</p>
                 <div class="button0239-item">
                     <a href="#">
-                        <button type="button" class="aboutus022">About Us</button>
-                    </a>
-                    <a href="#">
                         <button type="button" class="join92">Join Now</button>
                     </a>
-                </div>
-                <div class="form-style8292">
-                    <div class="input-group"> <span class="input-group-addon"><i class="fa fa-pencil-square" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control form-control8392" placeholder="Ask any question and you be sure find your answer ?"> <span class="input-group-addon"><a href="#">Ask Now</a></span> </div>
                 </div>
             </div>
         </div>
