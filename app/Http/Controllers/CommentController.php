f5598423673b4fccccc82fb2ad9a524010c78d0a
Haris F. <acc.cp.haris@gmail.com>
2024-11-10 10:56:13 +0700

    Add comment be

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $comment = $request->validate([
+            'content' => 'required',
+            'post_id' => 'required|exists:posts',
+            'user_id' => 'required|exists:users'
+        ]);
+
+        Comment::create($comment);
+        return true;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        $data = $request->validate([
+            'content' => 'required',
+            'post_id' => 'required|exists:posts',
+            'user_id' => 'required|exists:users'
+        ]);
+
+        $comment->content = $data['content'];
+        $comment->post_id = $data['post_id'];
+        $comment->user_id = $data['user_id'];
+        $comment->save();
+
+        return true;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+        return true;
+    }
+}
